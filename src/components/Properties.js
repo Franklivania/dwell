fa3fb4b47232418ccb9b089c87734f85bb54b64b
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-01-03 19:30:07 +0100

    finished panel one in properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -38,7 +38,48 @@ const Properties = () => {
 
     function handleClick(item) {
         setActiveItem(item);
+        setShowItems(false)
     }
+
+    const [house, setHouse] = useState([
+        { id:'0', name:'Duplex' },
+        { id:'1', name:'Detached House' },
+        { id:'2', name:'Semidetached House' },
+        { id:'3', name:'Bungalow' },
+        { id:'4', name:'Appartment' },
+        { id:'5', name:'Manor' },
+        { id:'6', name:'Mansion' },
+        { id:'7', name:'Estate' },
+        { id:'8', name:'Condo' },
+    ]);
+
+    const [showHouse, setShowHouse] = useState(false);
+    const [activeItem2, setActiveItem2] = useState(house[0]);
+
+    function handleClick2(item) {
+        setActiveItem2(item);
+        setShowHouse(false);
+    }
+
+    const [price, setPrice] = useState([
+        { id:'0', name:'$10,000' },
+        { id:'1', name:'$40,000' },
+        { id:'2', name:'$60,000' },
+        { id:'3', name:'$120,000' },
+        { id:'4', name:'$180,000' },
+        { id:'5', name:'$210,000' },
+        { id:'6', name:'$250,000' },
+        { id:'7', name:'$350,000' },
+    ]);
+
+    const [showPrice, setShowPrice] = useState(false);
+    const [activeItem3, setActiveItem3] = useState(price[0]);
+
+    function handleClick3(item) {
+        setActiveItem3(item);
+        setShowPrice(false);
+    }
+
   return (
     <div>
         <div className="container">
@@ -51,20 +92,54 @@ const Properties = () => {
             <div className="content">
 
                 <div style={{ display: activeDiv === 0 ? 'block' : 'none' }}>
-                    <div>
-                        <button onClick={() => setShowItems(!showItems)}>Location <i class="fa-solid fa-chevron-down"></i></button>
-                        {showItems && (
-                            <ItemList 
-                            items={items} 
-                            handleClick={handleClick} 
-                            activeItem={activeItem} 
-                            />
-                        )}
-                        {activeItem && (
-                            <ul>
-                                <li> {activeItem.name} </li>
-                            </ul>
-                        )}
+                    <div className='place'>
+                        <div className="card">
+                            <button onClick={() => setShowItems(!showItems)}>Location <i class="fa-solid fa-chevron-down"></i></button>
+                                {showItems && (
+                                    <ItemList 
+                                    items={items.filter(i => i !== activeItem)} 
+                                    handleClick={handleClick} 
+                                    activeItem={activeItem} 
+                                    />
+                                    )}
+                                {activeItem && (
+                                    <div style={{}}> {activeItem.name} </div>
+                                    )}
+                        </div>
+                        <div className='card'>
+                            <button onClick={() => setShowHouse(!showHouse)}>Property Type <i class="fa-solid fa-chevron-down"></i></button>
+                                {showHouse && (
+                                    <ItemList 
+                                    items={house.filter(i => i !== activeItem2)}
+                                    handleClick={handleClick2} 
+                                    activeItem={activeItem2} 
+                                    />
+                                    )}
+                                {activeItem2 && (
+                                    <div>
+                                    {activeItem2.name}
+                                </div>
+                                )}
+                        </div>
+                        <div className='card'>
+                            <button onClick={() => setShowPrice(!showPrice)}>Price Range <i class="fa-solid fa-chevron-down"></i></button>
+                                {showPrice && (
+                                    <ItemList 
+                                    items={price.filter(i => i !== activeItem3)}
+                                    handleClick={handleClick3} 
+                                    activeItem={activeItem3} 
+                                    />
+                                    )}
+                                {activeItem3 && (
+                                    <div>
+                                        {activeItem3.name}
+                                    </div>
+                                )}
+                        </div>
+
+                        <label htmlFor="">
+                            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i>Search</button>
+                        </label>
                     </div>
                 </div>
 
@@ -85,7 +160,7 @@ function ItemList(props){
     const { items, handleClick, activeItem } = props;
 
   return (
-    <div>
+    <div style={{ position: 'absolute', bottom: 0 }} className='list'>
       {items.map(item => (
         <div 
           key={item.id} 
