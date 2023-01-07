dec6802c8ad167d2d0e2883fc9d6daa01ed813ad
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2023-01-07 13:20:10 +0100

    completed tab view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../sass/Navbar.scss'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+
+
   return (
     <header className='navbar'>
         <img src={logo} alt={'company logo'} />
 
-        <nav>
-            <Link to='/' className='link'>Home</Link>
-            <Link to='/' className='link'>Rent</Link>
-            <Link to='/' className='link'>Agent</Link>
-            <Link to='/' className='link'>About us</Link>
-            <Link to='/' className='link'>Contact us</Link>
-        </nav>
+        <div className="nav">
+          <nav>
+              <Link to='/' className='link'>Home</Link>
+              <Link to='/' className='link'>Rent</Link>
+              <Link to='/' className='link'>Agent</Link>
+              <Link to='/' className='link'>About us</Link>
+              <Link to='/' className='link'>Contact us</Link>
+          </nav>
 
-        <nav className="social">
-            <a href="/" rel='noreferrer'><i class="fa-brands fa-facebook fa-2x"></i></a>
-            <a href="/" rel='noreferrer'><i class="fa-brands fa-instagram fa-2x"></i></a>
-            <a href="/" rel='noreferrer'><i class="fa-brands fa-twitter fa-2x"></i></a>
-        </nav>
+          <nav className="social">
+              <a href="/" rel='noreferrer'><i class="fa-brands fa-facebook"></i></a>
+              <a href="/" rel='noreferrer'><i class="fa-brands fa-instagram"></i></a>
+              <a href="/" rel='noreferrer'><i class="fa-brands fa-twitter"></i></a>
+          </nav>
+        </div>
+
+        <div className="mobilemenu" onClick={() => setOpen(!open)}>
+          <div className={`burger ${isOpen ? "burger--open" : ""}`}
+                onClick={() => setIsOpen(!isOpen)}>
+            <div className="line" />
+            <div className="line" />
+            <div className="line" />
+          </div>
+            {open && (
+            <div className="menu">
+              <nav className='links'>
+                <Link to='/' className='link'>Home</Link>
+                <Link to='/' className='link'>Rent</Link>
+                <Link to='/' className='link'>Agent</Link>
+                <Link to='/' className='link'>About us</Link>
+                <Link to='/' className='link'>Contact us</Link>
+              </nav>
+
+              <nav className="social">
+                <a href="/" rel='noreferrer'><i class="fa-brands fa-facebook"></i></a>
+                <a href="/" rel='noreferrer'><i class="fa-brands fa-instagram"></i></a>
+                <a href="/" rel='noreferrer'><i class="fa-brands fa-twitter"></i></a>
+              </nav>
+            </div>
+            )}
+        </div>
     </header>
   )
 }
